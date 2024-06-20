00b1d861a1eaae441e6fa1670869314cfeef0365
Srizan-mhrz <shrijan.mhrz@gmail.com>
2024-06-20 09:23:06 +0545

    update

---
+++ list.py
@@ -0,0 +1,41 @@
+# people=["reman","kala","kaliya","Roman","Apo"]
+# people.sort()
+# print(people)
+
+
+# data=[
+#     [12,23,34,45],
+#     [45,[[600],700],345,56]
+# ]
+# print(data[1][1][1])
+# print(data[1][1][0])
+# print(data[1][2])
+# print(data[0][1])
+ 
+# dat={'shrijan','shrijan','ramu','shyam'}
+# dat1={2,4,5,6,}
+# print(dat.union(dat1))
+
+# datas={
+#     'name':'shrijan',
+#     'address':'sorhakhutte',
+#     'age': 18,
+#     'contact':{
+#         'email':'shrijan.mhrz@gmail.com',
+#         'phone':{
+#         'home':12753763,
+#         'mobile':2344562657
+#         }
+#     }
+# }
+#  print(datas['contact']['email'])
+# print(datas['contact']['phone']['home'])
+# data={
+#     'name':'shrijan',
+#     'age':18,
+#     'address':'sorhakhutte'
+# }
+# print(data.get('names','key not found'))
+# print(data.keys())
+# print(data.values())
+# print(data['age'])
---
+++ me.py
@@ -0,0 +1,27 @@
+print("srijan maharjan")
+print("sorakhute")
+u=567456
+print(u)
+name=["ram","hari","bipplov"]
+print(name[1])
+users=["dreri","frgtht"]
+users[0]="biplov"
+print(users)
+lover=[12,34,45.45]
+lover[2]=11
+print(lover)
+users.append("moturam")
+print(users)
+
+users.append("motu")
+print(users)
+users.insert(1,"shrijan")
+print(users.pop(1))
+users.remove("shrijan")
+print(users)
+users.clear()
+print(users)
+
+
+people=["reman","kala","kaliya"]
+print(people)
